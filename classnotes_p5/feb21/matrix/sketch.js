d5a6957c6a5acd59f393f16ce61b202dbd2366c2
Katherine <kl4370@nyu.com>
2024-02-21 13:47:32 -0500

    added classnotes for feb21

---
+++ classnotes_p5/feb21/matrix/sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(800,800);
+  background(255);
+  noStroke();
+ // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+  fill(206,103,126); //mauve
+  rect(0, 0,80,80);
+
+  push(); //records the previous matrix coords
+  translate(0,100);
+  fill(91,187,209);
+  rect(0,0,80,80);
+  pop();//restores to the recent previous
+
+  push();
+  translate(100,0);
+  fill(232,141,103);//orange
+  rect(0,0,80,80);
+  pop();
+  
+}
